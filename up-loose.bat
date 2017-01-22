eb2e317b56f36bd9f8c5f76c17ba6d89add6f1f0
digitalpartisan <digital.partisan.86@gmail.com>
2017-01-21 16:58:48 -0800

    Added packed files and .bat utilities

---
+++ up-loose.bat
@@ -0,0 +1,2 @@
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\CheatBunkerDLC03" "K:\FO4 Mods\CheatBunkerFarHarbor\Data\Scripts\CheatBunkerDLC03"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\CheatBunkerDLC03" "K:\FO4 Mods\CheatBunkerFarHarbor\Data\Scripts\Source\User\CheatBunkerDLC03"
---
+++ up-packed.bat
@@ -0,0 +1,1 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerFarHarbor - Main.ba2" "K:\FO4 Mods\CheatBunkerFarHarbor\Data\CheatBunkerFarHarbor - Main.ba2"
